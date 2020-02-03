1138fb56268c478d351d80939dd54794716e6049
workOfYasir <58635184+workOfYasir@users.noreply.github.com>
2020-02-03 20:25:27 +0500

    Navigation to Login page and building up login page

--- app/src/main/java/com/hybird/city/MainActivity.java
+++ app/src/main/java/com/hybird/city/MainActivity.java
@@ -2,7 +2,10 @@ package com.hybird.city;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+  public void moveToLogin(View v){
+      Intent IntentObj = new Intent(this,Login.class);
+      startActivity(IntentObj);
+      this.finish();
+  }
 }
